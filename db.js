c6d16d54efeba6eece04607f6dbd8844c646668e
Yog Patel <yg371732@dal.ca>
2025-04-17 23:16:37 -0300

    Committing changes before pull

--- db.js
+++ db.js
@@ -1,13 +1,33 @@
+// const { Client } = require("pg");
+
+// const client = new Client({
+//   user: "yogpatel", // e.g., yogpatel
+//   host: "localhost",
+//   database: "my_scraper_db", // Make sure this matches the DB you created
+//   password: "", // If your local PostgreSQL has a password, add it here
+//   port: 5432,
+// });
+
+// client.connect();
+
+// module.exports = client;
+
 const { Client } = require("pg");
 
+// Use the external database URL from Render
 const client = new Client({
-  user: "yogpatel", // e.g., yogpatel
-  host: "localhost",
-  database: "my_scraper_db", // Make sure this matches the DB you created
-  password: "", // If your local PostgreSQL has a password, add it here
-  port: 5432,
+  connectionString: process.env.DB_URL, // Use the environment variable to store the connection string
+  ssl: {
+    rejectUnauthorized: false, // Required for Render's SSL connection
+  },
 });
 
-client.connect();
+client.connect()
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
 
 module.exports = client;

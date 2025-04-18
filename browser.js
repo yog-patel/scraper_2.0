c8de7f2d123a3263c04e100d09af05438ab188b0
Yog Patel <yg371732@dal.ca>
2025-04-18 00:26:58 -0300

    minor changes

--- browser.js
+++ browser.js
@@ -14,6 +14,23 @@
 //         defaultViewport: false
 //     });
 // }
+// const puppeteer = require("puppeteer");
+// const puppeteerExtra = require("puppeteer-extra");
+// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+// puppeteerExtra.use(StealthPlugin());
+
+// async function launchBrowser() {
+//   const browser = await puppeteerExtra.launch({
+//     headless: false,
+//     executablePath: puppeteer.executablePath(), // this ensures Chromium is found
+//   });
+//   return browser;
+// }
+
+// module.exports = {
+//     launchBrowser
+// };
 const puppeteer = require("puppeteer");
 const puppeteerExtra = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
@@ -21,13 +38,27 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteerExtra.use(StealthPlugin());
 
 async function launchBrowser() {
-  const browser = await puppeteerExtra.launch({
-    headless: false,
-    executablePath: puppeteer.executablePath(), // this ensures Chromium is found
-  });
-  return browser;
+  try {
+    // For GitHub Actions, need to run in headless mode
+    const browser = await puppeteerExtra.launch({
+      headless: true, // Set to true for GitHub Actions
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu'
+      ]
+    });
+    
+    console.log("Browser launched successfully");
+    return browser;
+  } catch (error) {
+    console.error("Error launching browser Yog:", error);
+    throw error;
+  }
 }
 
 module.exports = {
-    launchBrowser
+  launchBrowser
 };

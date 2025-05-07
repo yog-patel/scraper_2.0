f45189516fb3b616ebf303c7da0b3858a391b3dc
Yog Patel <yg371732@dal.ca>
2025-05-07 17:15:17 -0300

    xata connected and running

--- browser.js
+++ browser.js
@@ -1,36 +1,4 @@
-// const puppeteer = require("puppeteer");
-// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
-// // Initialize puppeteer with stealth plugin
-// puppeteer.use(StealthPlugin());
-
-// /**
-//  * Launches a browser instance with specified configurations
-//  * @returns {Promise<Browser>} Puppeteer browser instance
-//  */
-// async function launchBrowser() {
-//     return await puppeteer.launch({ 
-//         headless: false,
-//         defaultViewport: false
-//     });
-// }
-// const puppeteer = require("puppeteer");
-// const puppeteerExtra = require("puppeteer-extra");
-// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-
-// puppeteerExtra.use(StealthPlugin());
-
-// async function launchBrowser() {
-//   const browser = await puppeteerExtra.launch({
-//     headless: false,
-//     executablePath: puppeteer.executablePath(), // this ensures Chromium is found
-//   });
-//   return browser;
-// }
-
-// module.exports = {
-//     launchBrowser
-// };
 const puppeteer = require("puppeteer");
 const puppeteerExtra = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
--- db.js
+++ db.js
@@ -1,9 +1,47 @@
-const { XataClient } = require('@xata.io/client');
+const { Client } = require("pg");
 
-// Initialize Xata client
-const xata = new XataClient({
-  apiKey: 'xau_WRxpN60kT7cEqyx22FLMcH7tp7vkmwDr0',
-  databaseURL: 'https://Infinity-Free-Fire-s-workspace-ntm0uo.us-east-1.xata.sh/db/webnovelvault:main'
-});
+// const client = new Client({
+//   user: "yogpatel", // e.g., yogpatel
+//   host: "localhost",
+//   database: "my_scraper_db", // Make sure this matches the DB you created
+//   password: "", // If your local PostgreSQL has a password, add it here
+//   port: 5432,
+// });
 
-module.exports = xata;
+// client.connect();
+
+// module.exports = client;
+
+// const { Client } = require("pg");
+
+// Use the external database URL from Render
+// const client = new Client({
+//   connectionString: "postgresql://carnage:ABEUEEbCtea4a7nSpDOXdHgs8xIewl8F@dpg-d00pbvk9c44c73cj0gmg-a.virginia-postgres.render.com/scraped_info",//process.env.DB_URL,
+//   ssl: {
+//     rejectUnauthorized: false, // Render requires this for SSL
+//   },
+// });
+// const client = new Client({
+//   user: "ntm0uo", // e.g., yogpatel
+//   host: "us-east-1.sql.xata.sh",
+//   database: "webnovelvault:main", // Make sure this matches the DB you created
+//   password: "xau_WRxpN60kT7cEqyx22FLMcH7tp7vkmwDr0", // If your local PostgreSQL has a password, add it here
+//   port: 5432,
+// });
+
+  const client = new Client({
+    connectionString: "postgresql://ntm0uo:xau_WRxpN60kT7cEqyx22FLMcH7tp7vkmwDr0@us-east-1.sql.xata.sh/webnovelvault:main?sslmode=require",//process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false, // Render requires this for SSL
+    },
+  });
+
+client.connect()
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
+
+module.exports = client;
